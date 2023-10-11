379f5f59b3ebd8303657de68893f9e4834e17397
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-10-11 14:01:26 +0000

    puppet script solves web stack error 500

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-#Puppet script to solve server error with help from strace
+# Puppet script to solve server error with help from strace
 
 exec { 'fix-wordpress':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
